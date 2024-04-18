9b102b63da64cbf54036c1a7af035f1d2852c125
Geekuer <120203378+Geekuer@users.noreply.github.com>
2024-04-18 21:51:23 +0900

    Add files via upload

---
+++ darkmode.js
@@ -0,0 +1,35 @@
+// 다크 모드를 토글하는 함수
+function toggleDarkMode() {
+	var btn = document.getElementById("button");
+	document.body.classList.toggle("dark-mode");
+	if (btn.innerHTML === "Night") {
+		btn.innerHTML = "Day";
+		saveDarkModeState(true); // 다크 모드가 켜진 상태를 저장
+	} else {
+		btn.innerHTML = "Night";
+		saveDarkModeState(false); // 다크 모드가 꺼진 상태를 저장
+	}
+}
+
+// 다크 모드 토글 상태를 저장하는 함수
+function saveDarkModeState(isDarkMode) {
+	localStorage.setItem('darkMode', isDarkMode);
+}
+
+// 다크 모드 토글 상태를 불러오는 함수
+function loadDarkModeState() {
+	return JSON.parse(localStorage.getItem('darkMode'));
+}
+
+
+// 페이지가 로드될 때 실행되는 함수
+window.onload = function() {
+	// 웹 스토리지에서 다크 모드 토글 상태를 불러옴
+	var isDarkMode = loadDarkModeState();
+
+	// 만약 다크 모드 토글 상태가 true이면 다크 모드를 적용
+	if (isDarkMode) {
+		document.body.classList.add("dark-mode");
+		document.getElementById("button").innerHTML = "Day";
+	}
+};
